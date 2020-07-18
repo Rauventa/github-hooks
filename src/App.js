7edfc922d2f83da88f7a237e97063d8027db3c6a
Rauventa <rauventa@gmail.com>
2020-07-18 12:01:52 +0300

    API added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+        <h3>{this.props.propsCounter}</h3>
+        <button onClick={this.props.onAddCounter}>Add 1</button>
+        <button onClick={this.props.onSubCounter}>Sub 1</button>
+      </div>
+    )
+  }
 }
 
-export default App;
+// В этой функции мы должны забирать данные из state reducer, и записывать их в отдельную переменную, к который мы потом будем оьращаться через props
+const mapStateToProps = (state) => ({
+  propsCounter: state.counter
+})
+
+// В этой функции должны быть записи того, как будут выглядть функции в actions (Храним тут dispatch'и)
+const mapDispatchToProps = (dispatch) => ({
+  onAddCounter: () => dispatch({type: 'ADD'}),
+  onSubCounter: () => dispatch({type: 'SUB'})
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
